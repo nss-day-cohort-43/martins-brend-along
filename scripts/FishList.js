1963dd1f64cfe8de898f57cab3e73718f70885a5
Parker Kelley <Pklly92@gmail.com>
2020-08-31 16:08:56 -0500

    made the html code use map

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -2,22 +2,17 @@
  *  FishList which renders individual fish objects as HTML
  */
 
-import { useFish } from "./FishDataProvider.js";
-import { Fish } from "./Fish.js";
+import { useFish } from './FishDataProvider.js';
+import { Fish } from './Fish.js';
 
 export const FishList = () => {
 	// Get a reference to the `<article class="content">` element
-	const contentElement = document.querySelector(".fishList");
+	const contentElement = document.querySelector('.fishList');
 	const fishes = useFish();
-
-	let fishHTMLRepresentation = "";
-	for (const fish of fishes) {
-		fishHTMLRepresentation += Fish(fish);
-	}
 
 	// Add to the existing HTML in the content element
 	contentElement.innerHTML += `
     
-        ${fishHTMLRepresentation}
+        ${fishes.map((fish) => Fish(fish)).join('')}
     `;
 };
