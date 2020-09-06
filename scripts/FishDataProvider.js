d44e8662e2b97272abb2b8ad0e16bdcc1bdaab8e
Dani Crosby <dani.crosby1111@gmail.com>
2020-09-06 13:38:12 -0500

    test

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,9 +1,4 @@
-/*
- *  To get you started, here's some properties of Bart.
- *  You need to add more properties to complete his
- *  representation as an object. Then add all the other
- *  fish to the collection.
- */
+
 const fishCollection = [
     {
 		givenName: "Bart",
